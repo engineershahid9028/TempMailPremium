264b50526422c14aa2640d7633696fce17cde1fc
engineershahid9028 <141816607+engineershahid9028@users.noreply.github.com>
2026-01-15 00:55:24 +0500

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.settings import DATABASE_URL
-from app.models import Base, User, EmailVault, Payment
+from settings import DATABASE_URL
+from models import Base, User, EmailVault, Payment
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
